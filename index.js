4c919559b4c26ba5aa78afa186368be667a92b41
Dioonel <dionelmartinezc@gmail.com>
2022-03-18 16:27:55 -0300

    Details of the requested movie are now displayed, such as title, flyer (if available), actors, etc.

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const KEY = `a201b035`;                                                  // public API key
 const input = document.getElementById("input");
 const button = document.getElementById("button");
+const image = document.getElementById("poster");
 
+const title = document.getElementById("title");
+const year = document.getElementById("year");
+const genres = document.getElementById("genres");
+const actors = document.getElementById("actors");
 
 async function request(url){
     const response = await fetch(url);
@@ -12,10 +17,18 @@ async function request(url){
 async function executeRequest(){
     let movieName = input.value;
     const url = `http://www.omdbapi.com/?t=${movieName}&plot=full&apikey=${KEY}`;
-    console.log(await request(url));
+    const response = await request(url);
+    console.log(response);
+    decorateHTML(response);
 }
 
-
+function decorateHTML(res){
+    title.innerHTML = `<strong>MOVIE TITLE:</strong> ${res.Title}`;
+    year.innerHTML = `<strong>YEAR:</strong> ${res.Year}`;
+    genres.innerHTML = `<strong>GENRES:</strong> ${res.Genre}`;
+    actors.innerHTML = `<strong>ACTORS:</strong> ${res.Actors}`;
+    image.src = res.Poster;
+}
 
 
 
