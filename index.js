56b025f747f5a0cebd89689a761df84fa181ce50
Dioonel <dionelmartinezc@gmail.com>
2022-03-18 15:47:10 -0300

    Search by Title added, visible in console with some random errors

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
 const key = `a201b035`;
-const url = `http://www.omdbapi.com/?t=Spider+man&plot=full&apikey=${key}`
+//const url = `http://www.omdbapi.com/?t=${title}&plot=full&apikey=${key}`;
 
-async function request(){
+const input = document.getElementById("input");
+const button = document.getElementById("button");
+
+
+let search = () => {
+    let movieName = input.value;
+    const url = `http://www.omdbapi.com/?t=${movieName}&plot=full&apikey=${key}`;
+    request();
+    executeRequest(url);
+}
+
+async function request(url){
     const response = await fetch(url);
     const data = response.json();
     return data;
 }
 
-async function execute(){
-    console.log(await request());
+async function executeRequest(urlXD){
+    console.log(await request(urlXD));
 }
 
-execute();
+//execute();
+
+
+
+
+
+
 
